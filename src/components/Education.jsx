209f05bfb4ff1f2939f43d76474f618ba44d96be
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2021-01-13 11:37:59 +0000

    Post recalling Miranda and Warwick

---
+++ src/components/Education.jsx
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+import {jsx} from "theme-ui"
+import PropTypes from 'prop-types';
+
+const UniLink = (props) => {
+    return(<a href={props.url}>{props.children ? props.children : props.name}</a>)
+}
+
+UniLink.propTypes = {
+    url: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+}
+
+export const Warwick = (props) => {
+    return(
+        <UniLink url='https://warwick.ac.uk/' name='University of Warwick'>{props.children}</UniLink>
+    )
+}
+
+export const Maths = (props) => {
+    return(
+        <UniLink url='https://warwick.ac.uk/fac/sci/maths/' name='Maths Institute'>{props.children}</UniLink>
+    )
+}
+
+export const PlateGlassUni = (props) => {
+    return(
+        <UniLink url='https://en.wikipedia.org/wiki/Plate_glass_university' name='plate glass'>{props.children}</UniLink>
+    )
+}
+
+
+export const RussellGroup = (props) => {
+    return(
+        <UniLink url='https://russellgroup.ac.uk/' name='Russell Group'>{props.children}</UniLink>
+    )
+}
