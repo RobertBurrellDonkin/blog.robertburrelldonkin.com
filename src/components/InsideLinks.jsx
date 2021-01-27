8d2b53512459d764914ae299d4a813583704fc1f
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2021-01-27 09:59:25 +0000

    GH118: Edited post aboutkafka and the incubator

---
+++ src/components/InsideLinks.jsx
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import {jsx} from "theme-ui"
+
+import {Link} from "gatsby"
+
+
+export function ApacheDays() {
+    return (
+        <Link to='/memberships/asf'>Apache days</Link>
+    )
+}
